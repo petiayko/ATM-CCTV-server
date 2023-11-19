7b9678c8abe47ae8583d39c48a7ea5809718637d
Peter <minmen2013@yandex.ru>
2023-11-19 23:44:34 +0300

    SV: Implement easiest django app

---
+++ cameras/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+def configuration_default():
+    return {
+        'ip_address': '192.168.17.19',  # ipv4 camera address
+        'prerecord_time': 5,  # prerecord time in secs
+        'postrecord_time': 5,  # postrecord time in secs
+    }
+
+
+class Camera(models.Model):
+    name = models.CharField(verbose_name='Имя камеры')
+    configuration = models.JSONField(verbose_name='Конфигурация камеры', default=configuration_default)
+
+    class Meta:
+        ordering = 'name',
+        verbose_name = 'Камера'
+        verbose_name_plural = 'Камеры'
+
+    def __str__(self):
+        return self.name
