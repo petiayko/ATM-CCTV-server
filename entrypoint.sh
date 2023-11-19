1c370a81c99a07f0782859c0071a122aeb72a558
Peter <minmen2013@yandex.ru>
2023-11-20 00:05:22 +0300

    SV: Add docker files

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export IS_DOCKER_COMPOSE=1
+cd ATM-CCTV-server
+python manage.py migrate
+python manage.py loaddata fixtures/users.json
+printf "\n\n\t\tsuperuser created:\n\t\t\tlogin - admin\n\t\t\tpassword - 8Xgex7E6CRCF4wz\n\n\n"
+python manage.py runserver 0.0.0.0:8000
+
+exec $cmd
